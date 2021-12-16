7f17632c796220a7a93a0707971f763ebdb099ae
Jökull Sólberg Auðunsson <jokull@solberg.is>
2021-12-16 15:14:18 +0000

    Add tryinch.com link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,23 +45,19 @@
 		<div class="order-1 sm:order-0 text-sm sm:text-base">
 			<div class="introduction">
 				<p class="mb-3">
-					Ég er vörustjóri hjá <a class="font-bold" href="https://www.getsling.com">Sling</a> og
-					vinn að nýrri vöru þar sem heitir
-					<a
-						class="font-bold"
-						href="https://www.craft.do/s/0ZbFxiNPQ7j79d?fbclid=IwAR2hWTdZrhfE8iOPU33OcgyWDbky9-16XUJbBZGS67hLHS0JZuMfVO_s6JM"
-						>Inch</a
-					>. Ég hef starfað við ráðgjöf, meðal annars fyrir
-					<a class="font-bold" href="https://www.island.is/">Stafrænt Ísland</a>. Ég er stofnandi
-					<a class="font-bold" href="https://www.planitor.io/">Planitor</a> og
-					<a class="font-bold" href="https://takumi.com/">Takumi International ltd</a>. Þar á undan
-					starfaði ég sem forritari og vörustjóri hjá
-					<a class="font-bold" href="https://quizup.com">QuizUp</a>.
+					Ég er vörustjóri hjá <a href="https://www.getsling.com">Sling</a> og vinn að nýrri vöru
+					þar sem heitir
+					<a href="https://www.tryinch.com/">Inch</a>. Ég hef starfað við ráðgjöf, meðal annars
+					fyrir
+					<a href="https://www.island.is/">Stafrænt Ísland</a>. Ég er stofnandi
+					<a href="https://www.planitor.io/">Planitor</a> og
+					<a href="https://takumi.com/">Takumi International ltd</a>. Þar á undan starfaði ég sem
+					forritari og vörustjóri hjá
+					<a href="https://quizup.com">QuizUp</a>.
 				</p>
 				<p>
-					Ég hélt úti fréttabréfinu <a class="font-bold" href="https://jokull.substack.com/"
-						>Reykjavík Mobility</a
-					> þar sem fjallað er um samgöngur og skipulag í Reykjavík.
+					Ég hélt úti fréttabréfinu <a href="https://jokull.substack.com/">Reykjavík Mobility</a> þar
+					sem fjallað er um samgöngur og skipulag í Reykjavík.
 				</p>
 			</div>
 		</div>
