5c443f77b07432e3f2ad430a1e9b7b1ddb37bdef
Jökull Sólberg <jokull@solberg.is>
2022-04-27 18:47:02 +0000

    url &&

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,7 +8,7 @@
 <script>
 	export let url;
 	export let books;
-	export let slug = url.pathname.split('/').at(-1);
+	export let slug = url && url.pathname.split('/').at(-1);
 	import Books from '../components/Books.svelte';
 	import '../app.css';
 </script>
